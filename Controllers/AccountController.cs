c8c000842c59086b543561dac39e7a03e288f169
veronica0713 <bayarynava@gmail.com>
2018-05-03 11:35:32 -0500

    added Registration
    added Registration

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyStore.Models;
+
+namespace MyStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Responds on GET/Account/Register:
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //Responds on POST/Account/Register:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Create an account and log this user in:
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MyStore.Models
+{
+    public class RegisterViewModel
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.MinLength(5)]
+        [System.ComponentModel.DataAnnotations.MaxLength(10)]
+        public string UserName { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Password { get; set; }
+    }
+}
